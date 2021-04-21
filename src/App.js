29f6d11d826ddefa644583315cb07da6aac8e0e3
SoHo21 <79358586+SoHo21@users.noreply.github.com>
2021-04-21 10:25:36 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container"> 
       <header className="App-header">
       Dictionary
       </header>
-      <footer>
-        This project was coded by Sofia and is <a href="https://github.com/SoHo21/dictionary-project" target="_blank" rel="noreferrer"> open-sourced on GitHub </a>  and <a href="https://angry-spence-c7ac4e.netlify.app/" target="_blank" rel="noreferrer"> hosted on Netlify </a>.
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        Coded by Sofia. <a href="https://github.com/SoHo21/dictionary-project" target="_blank" rel="noreferrer"> Open-sourced on GitHub </a>  and <a href="https://angry-spence-c7ac4e.netlify.app/" target="_blank" rel="noreferrer"> hosted on Netlify</a>.
       </footer>
+      </div>
     </div>
   );
 }
