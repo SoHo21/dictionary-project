c89b58ce8e17ee2dfd3b6cadb1caedfdc33ebfa3
SoHo21 <79358586+SoHo21@users.noreply.github.com>
2021-04-21 09:46:02 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Dictionary
+      </header>
+      <footer>
+        This project was coded by Sofia and is open-sourced on GitHub and hosted on Netlify.
+      </footer>
+    </div>
+  );
+}
