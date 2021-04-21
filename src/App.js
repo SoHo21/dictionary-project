f9a94aa0dc0cd0a21bb6070cf11a9633ad81f12e
SoHo21 <79358586+SoHo21@users.noreply.github.com>
2021-04-21 09:54:53 +0200

    Added links

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       Dictionary
       </header>
       <footer>
-        This project was coded by Sofia and is open-sourced on GitHub and hosted on Netlify.
+        This project was coded by Sofia and is <a href="https://github.com/SoHo21/dictionary-project" target="_blank" rel="noreferrer"> open-sourced on GitHub </a>  and <a href="https://angry-spence-c7ac4e.netlify.app/" target="_blank" rel="noreferrer"> hosted on Netlify </a>.
       </footer>
     </div>
   );
